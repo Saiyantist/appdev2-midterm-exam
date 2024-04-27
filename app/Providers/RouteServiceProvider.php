08170ce4fb7788f8ac2ed2b13ff9bb5462c5c3db
Gelo <angelo.delossantos000@gmail.com>
2024-04-27 11:14:01 +0800

    test

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('api')
+            ->prefix('friday')
+            ->group(base_path('routes/friday.php'));
         });
     }
 
